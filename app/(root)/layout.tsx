39e72114e5d2400f143edf3a0c1885680ff87bfe
Dev-scott <sadoscott25@gmail.com>
2023-08-28 20:42:38 +0100

    update

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+import { Sidebar } from "@/components/Sidebar";
+import { NavBar } from "@/components/navbar";
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full">
+        <NavBar/>
+        <div className="hidden md:flex mt-16 w-20 flex-col fixed inset-y-0" >
+            <Sidebar/>
+        </div>
+      <main className="md:pl-20 pt-16 h-full">{children}</main>
+    </div>
+  );
+};
+
+export default RootLayout;
